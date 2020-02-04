fe40f5a5933b96c322afd2d9dd46601faee11d8b
Jens Grønhøj Stigaard <jens@stjernholmco.dk>
2020-02-04 15:10:45 +0100

    Updated export of modules

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,18 @@ import ConnectionTCP from './modules/connection-tcp'
 import ConnectionHTTP from './modules/connection-http'
 import CommandSenderHTTP from './modules/command-sender-http'
 
+const Connection = ConnectionTCP
+
+export {
+	Connection, // Alias for ConnectionTCP
+	ConnectionTCP,
+
+	CommandSenderHTTP,
+	ConnectionHTTP
+}
+
 export default {
+	Connection, // Alias for ConnectionTCP
 	ConnectionTCP,
 
 	// Legacy classes: 
