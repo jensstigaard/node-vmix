7169307ef6199c1d3d47faef6096a60429601052
Jens Stigaard <jens@stigaard.info>
2021-03-04 15:51:28 +0100

    Update send-multiple-commands-mixed.js

--- examples/send-multiple-commands-mixed.js
+++ examples/send-multiple-commands-mixed.js
@@ -5,27 +5,30 @@ const { ConnectionTCP } = require('../dist/index')
 // Instanciate connection to vMix instance via TCP localhost:8099
 const connection = new ConnectionTCP('localhost', '8089')
 
-connection.on('connect', () => {
-  console.log('Connected')
-})
 connection.on('error', (error) => {
   console.error('Error', error)
 })
 connection.on('data', data => {
   console.log('Got response with data:', data)
 })
-// Perform commands directly on connection
-// You can use any vMix Function here
-// List of all functions here: 
-// https://www.vmix.com/help22/ShortcutFunctionReference.html
 
-// Perform multiple commands at once:
-connection.send([
-  'TALLY', // Get tally information
-  // - On title on input with name 'MyTitle' - set text of field "TitleField" to the text "Updated text!"
-  { Function: 'SetText', Input: 'MyTitle', SelectedName: 'TitleField', Value: 'Updated text!' },
-  // - Put input 1 on air on overlay channel 1
-  { Function: 'OverlayInput1On', Input: 'MyTitle' },
-  // - Perform Cut to the input currently in preview
-  { Function: 'Cut' }
-])
+
+connection.on('connect', () => {
+  console.log('Connected')
+  
+  // Perform commands directly on connection as soon as connected
+  // You can use any vMix Function here
+  // List of all functions here: 
+  // https://www.vmix.com/help22/ShortcutFunctionReference.html
+
+  // Perform multiple commands at once:
+  connection.send([
+    'TALLY', // Get tally information
+    // - On title on input with name 'MyTitle' - set text of field "TitleField" to the text "Updated text!"
+    { Function: 'SetText', Input: 'MyTitle', SelectedName: 'TitleField', Value: 'Updated text!' },
+    // - Put input 1 on air on overlay channel 1
+    { Function: 'OverlayInput1On', Input: 'MyTitle' },
+    // - Perform Cut to the input currently in preview
+    { Function: 'Cut' }
+  ])
+})
