cd71028684774a4e80ec935705ab0ccf7b5669a9
Jens Stigaard <jens@stigaard.info>
2021-05-15 14:26:20 +0200

    Added scratch file

---
+++ scratch/native-socket.ts
@@ -0,0 +1,22 @@
+import Socket from 'net'
+
+const client = Socket.createConnection(
+	{ host: 'localhost', port: 8099 },
+	() => { console.log('Msg upon initiazation') }
+)
+
+client.on('connect', (err: any) => {
+	if (err) {
+		console.error(err)
+		return
+	}
+
+	console.log('Connected!')
+})
+
+client.on('data', (data: Buffer) => {
+
+	const str = data.toString('utf8')
+
+	console.log('Received data!', str)
+})
